244c3106ad63d1031050b8766f7a623f1a46de9b
zhengxian.lzx <lizhengxian1991@126.com>
2017-09-07 01:27:07 +0800

    drawer 控件OK

--- app/src/main/java/top/lizhengxian/drawable/drawable/HomeActivity.java
+++ app/src/main/java/top/lizhengxian/drawable/drawable/HomeActivity.java
@@ -6,7 +6,10 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,5 +20,8 @@ public class HomeActivity extends BaseActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        new DrawerBuilder()
+                .withToolbar(toolbar).withActivity(this).build();
     }
 }
