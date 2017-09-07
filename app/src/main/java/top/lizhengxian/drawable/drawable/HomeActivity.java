c2b92fb108feba3c46a10239af95522933816177
zhengxian.lzx <lizhengxian1991@126.com>
2017-09-07 10:06:23 +0800

    add drawer item with menu ids

--- app/src/main/java/top/lizhengxian/drawable/drawable/HomeActivity.java
+++ app/src/main/java/top/lizhengxian/drawable/drawable/HomeActivity.java
@@ -9,19 +9,49 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class HomeActivity extends BaseActivity {
+    private Drawer mDrawer;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        new DrawerBuilder()
-                .withToolbar(toolbar).withActivity(this).build();
+        AccountHeader accountHeader = new AccountHeaderBuilder().withTranslucentStatusBar(true)
+                .addProfiles(new ProfileDrawerItem().withIcon(R.drawable.ic_home_tab_market_selector).withName("hello"))
+                .withDividerBelowHeader(false)
+                .withActivity(this)
+                .build();
+        mDrawer = new DrawerBuilder()
+                .withToolbar(toolbar)
+                .withActivity(this)
+                .inflateMenu(R.menu.drawer)
+                .withTranslucentStatusBar(true)
+                .withAccountHeader(accountHeader)
+                .withMultiSelect(false)
+                .withSelectedItemByPosition(-1)
+                .build();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen()) {
+            mDrawer.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
