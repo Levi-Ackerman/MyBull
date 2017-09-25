5d95db4169ac9d515308a2de7bdb1539352766d0
zhengxian.lzx <lizhengxian1991@126.com>
2017-09-25 22:54:31 +0800

    add multiFragments

---
+++ multifragments/src/main/java/top/lizhengxian/multifragments/BaseActivity.java
@@ -0,0 +1,10 @@
+package top.lizhengxian.multifragments;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhengxianlzx on 17-9-24.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ multifragments/src/main/java/top/lizhengxian/multifragments/BaseFragment.java
@@ -0,0 +1,32 @@
+package top.lizhengxian.multifragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by zhengxianlzx on 17-9-24.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    protected View mRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        if (mRootView == null){
+            mRootView = inflater.inflate(getLayoutId(),container,false);
+            ButterKnife.bind(this, mRootView);
+        }
+        return mRootView;
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutId() ;
+}
