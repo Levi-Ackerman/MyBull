2a3dc40e4bf06621ff682d6b7599cf11220d7f0a
zhengxian.lzx <lizhengxian1991@126.com>
2017-09-27 20:59:53 +0800

    删删干净

--- app/src/main/java/top/lizhengxian/drawable/drawable/HomeActivity.java
+++ app/src/main/java/top/lizhengxian/drawable/drawable/HomeActivity.java
@@ -1,70 +1,36 @@
 package top.lizhengxian.drawable.drawable;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-
-import java.util.List;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class HomeActivity extends BaseActivity {
-    private Drawer mDrawer;
+    protected Toolbar mToolbar;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_vector_menu));
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(getDrawable(R.drawable.ic_vector_menu));
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 return false;
             }
         });
-        mDrawer = new DrawerBuilder()
-                .withActivity(this)
-                .withToolbar(toolbar)
-                .withHeader(R.layout.view_drawable)
-                .withHeaderDivider(false)
-                .inflateMenu(R.menu.drawer)
-//                .withAccountHeader(accountHeader)
-                .withMultiSelect(false)
-                .withSelectedItemByPosition(-1)
-                .build();
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.title_bar,menu);
         return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mDrawer.isDrawerOpen()) {
-            mDrawer.closeDrawer();
-        } else {
-            super.onBackPressed();
-        }
     }
 }
